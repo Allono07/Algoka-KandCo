6e9883bb3cda4313e3d0b4c445c48570e750129f
Allono07 <allono.at@gmail.com>
2026-06-15 15:34:16 +0530

    client logos

--- src/data/clients.js
+++ src/data/clients.js
@@ -1,11 +1,17 @@
-// Placeholder client logos – replace with actual SVG URLs
+import client1 from '../assets/client_logos/IMG_6300.jpg'
+import client2 from '../assets/client_logos/IMG_6358.png'
+import client3 from '../assets/client_logos/PHOTO-2026-06-08-13-40-23.jpg'
+import client4 from '../assets/client_logos/PHOTO-2026-06-10-14-36-25.jpg'
+import client5 from '../assets/client_logos/PHOTO-2026-06-10-14-37-34.jpg'
+import client6 from '../assets/client_logos/PHOTO-2026-06-10-14-41-32.jpg'
+import client7 from '../assets/client_logos/PHOTO-2026-06-10-16-05-21.jpg'
+
 export const clients = [
-  { name: 'Client 1', logo: '/src/assets/images/clients/client-1.svg' },
-  { name: 'Client 2', logo: '/src/assets/images/clients/client-2.svg' },
-  { name: 'Client 3', logo: '/src/assets/images/clients/client-3.svg' },
-  { name: 'Client 4', logo: '/src/assets/images/clients/client-4.svg' },
-  { name: 'Client 5', logo: '/src/assets/images/clients/client-5.svg' },
-  { name: 'Client 6', logo: '/src/assets/images/clients/client-6.svg' },
-  { name: 'Client 7', logo: '/src/assets/images/clients/client-7.svg' },
-  { name: 'Client 8', logo: '/src/assets/images/clients/client-8.svg' },
+  { name: 'Client 1', logo: client1 },
+  { name: 'Client 2', logo: client2 },
+  { name: 'Client 3', logo: client3 },
+  { name: 'Client 4', logo: client4 },
+  { name: 'Client 5', logo: client5 },
+  { name: 'Client 6', logo: client6 },
+  { name: 'Client 7', logo: client7 },
 ];
